db57cc1d42543654c51bcd555fcbf2495b047a9d
unknown <20it113@charusat.edu.in>
2022-09-13 12:03:22 +0530

    Added Delete

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -17,6 +17,17 @@ export default function StudentList() {
       .then((response) => setStudents(response.data))
       .catch((error) => alert(error));
   };
+
+  let deleteStudent = (studentId) => {
+    axios.delete("http://localhost:8080/student/"+studentId)
+    .then(response=> {
+      if (response.data !== null){
+        alert("Record Deleted Successfully");
+        
+      }
+    })
+  }
+
   return (
     <div className="my-3">
       <Container>
@@ -40,14 +51,15 @@ export default function StudentList() {
                 </tr>
               ) : (
                 students.map((student)=>
-                <tr>
+                <tr key={student.id}>
                   <td>{student.id}</td>
                   <td>{student.name}</td>
                   <td>{student.address}</td>
                   <td>
                     <ButtonGroup>
                       <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
-                      <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                      <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this,student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                      {/* <Button size="sm" variant="outline-danger" onClick={()=>deleteStudent(student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button> */}
                     </ButtonGroup>
                   </td>
                 </tr>
