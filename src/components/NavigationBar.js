0de1e5cfbfd5074037de845ffe21058b0f549f96
unknown <20it113@charusat.edu.in>
2022-09-13 12:02:03 +0530

    Added Routing

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,23 +1,62 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useEffect, useState } from "react";
+import { Card, Container, Table, ButtonGroup, Button } from "react-bootstrap";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export default function NavigationBar() {
+export default function StudentList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  let getStudents = () => {
+    axios
+      .get("http://localhost:8080/listStudents")
+      .then((response) => setStudents(response.data))
+      .catch((error) => alert(error));
+  };
   return (
-    <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <div className="my-3">
       <Container>
-        <Navbar.Brand href="#home">Student Management System</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Add Student</Nav.Link>
-            <Nav.Link href="#pricing">View Students</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Card.Header>
+          <h3>Students List</h3>
+        </Card.Header>
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Student Id</th>
+                <th>Student Name</th>
+                <th>Student Address</th>
+                <th>Edit/Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.length === 0 ? (
+                <tr>
+                  <td colSpan={3}>{students.length} Studnets Available!!!</td>
+                </tr>
+              ) : (
+                students.map((student)=>
+                <tr key={student.id}>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.address}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
+                      <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+                )
+              )}
+            </tbody>
+          </Table>
+        </Card.Body>
       </Container>
-    </Navbar>
     </div>
-  )
+  );
 }
