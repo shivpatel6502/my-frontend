7badfbf5bc82234348164a5e5a7d51b45f2dc6be
unknown <20it113@charusat.edu.in>
2022-09-13 11:57:32 +0530

    Added Students Listing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Student from './components/Student';
+import StudentList from './components/StudentList';
+import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
+import Container from 'react-bootstrap/Container';
+  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <NavigationBar/>
+        <Container>
+     <Student/>
+      <StudentList/>
+      </Container>
+      <Footer/>
     </div>
   );
 }
